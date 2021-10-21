ff86c57d1ff01f8f275b0ccab693a0b2cbe929c5
iamthem <lslopez@andrew.cmu.edu>
2021-10-21 00:25:21 -0400

    new brain

--- hw2_code/pytorch/a2c/net.py
+++ hw2_code/pytorch/a2c/net.py
@@ -10,8 +10,9 @@ class Reinforce_Loss(torch.nn.NLLLoss):
         return torch.div(torch.dot(G, nll_result), T)
 
 # TODO Consider ignoring output_activation, and using L = torch.nn.CrossEntropyLoss
+#
 class NeuralNet(torch.nn.Module):
-    def __init__(self, input_size, output_size, activation, layers=[32,32,16]):
+    def __init__(self, input_size, output_size, activation, layers=[64, 64, 64, 32, 32, 16]):
         super().__init__()
 
         self.linear1 = torch.nn.Linear(input_size, layers[0])
@@ -20,8 +21,14 @@ class NeuralNet(torch.nn.Module):
         self.activation2 = torch.nn.ReLU()
         self.linear3 = torch.nn.Linear(layers[1], layers[2])
         self.activation3 = torch.nn.ReLU()
+        self.linear4 = torch.nn.Linear(layers[2], layers[3])
+        self.activation4 = torch.nn.ReLU()
+        self.linear5 = torch.nn.Linear(layers[3], layers[4])
+        self.activation5 = torch.nn.ReLU()
+        self.linear6 = torch.nn.Linear(layers[4], layers[5])
+        self.activation6 = torch.nn.ReLU()
 
-        self.output_layer = torch.nn.Linear(layers[2], output_size)
+        self.output_layer = torch.nn.Linear(layers[5], output_size)
         self.output_activation = activation
 
         #initialize weights, following 'fan_avg' approach
@@ -34,6 +41,9 @@ class NeuralNet(torch.nn.Module):
         x = self.activation1(self.linear1(inputs))
         x = self.activation2(self.linear2(x))
         x = self.activation3(self.linear3(x))
+        x = self.activation4(self.linear4(x))
+        x = self.activation5(self.linear5(x))
+        x = self.activation6(self.linear6(x))
         x = self.output_activation(self.output_layer(x))
         # logger.debug("output of NN ==> %s", str(x))
         # logger.debug("output of argmax(x) ==> %s", str(torch.argmax(x)))
